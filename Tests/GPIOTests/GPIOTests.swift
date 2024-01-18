0a2739bcf54083c03c30da4d5ac551d634ac4574
Vaidios <kamil.karol.sosna@gmail.com>
2024-01-18 22:38:10 +0000

    Added a test for the consumer, at least I will know when it will work :E

--- Tests/GPIOTests/GPIOTests.swift
+++ Tests/GPIOTests/GPIOTests.swift
@@ -20,4 +20,34 @@ final class GPIOTests: XCTestCase {
         XCTAssertEqual(mask, 0b1010, "Bits 1 and 3 should be set")
         // Add more test cases as needed
     }
+
+    func testGpiodLineRequest() throws {
+                let settings = Line.Settings(
+            direction: .output, 
+            edge: .falling, 
+            drive: .openDrain, 
+            bias: .disabled, 
+            activeLow: true, 
+            eventClock: .realtime, 
+            debouncePeriod: 2, 
+            outputValue: .active
+        )
+        let consumer = "test"
+        let configuration = RequestConfiguration(consumer: consumer, eventBufferSize: 1)
+        let lineConfiguration = Line.Configuration(offset: 0, settings: settings)
+        let bulkConfiguration = Line.BulkConfiguration(configs: [lineConfiguration], values: [.active])
+
+        for path in try Chip.allChipPaths() {
+            let chip = try Chip(path: path)
+
+            guard let request = chip.requestLines(lineBulkConfiguration: bulkConfiguration, requestConfiguration: configuration) else { return }
+            let info = chip.info()
+            for index in 0 ..< info.lines {
+                if index == 0 {
+                    let lineInfo = chip.lineInfo(offset: index)
+                    XCTAssertEqual(lineInfo.consumer, consumer)
+                }
+            }
+        }
+    }
 }
